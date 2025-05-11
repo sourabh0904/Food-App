53e69bd0c42a18f3e67c09d585dcf6151951d1b9
sourabh0904 <patidarsourabh687@gmail.com>
2025-05-11 16:52:02 +0530

    Error page added

---
+++ src/components/Error.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <h3>Sorry, the page you are looking for does not exist.</h3>
+      <h4>Please check the URL or go back to the homepage.</h4>
+      <h5>Thank you for your understanding.</h5>
+      <h6>Contact us if you need further assistance.</h6>
+      <h6>We are here to help you!</h6>
+      <h6>Have a great day!</h6>
+      <h6>Best regards,</h6>
+    </div>
+  );
+};
+
+export default Error;
